0b8abe73281c64f054ac2d95d76de0dc60bed100
Filipe Rosset <rosset.filipe@gmail.com>
2021-02-05 17:35:53 -0300

    Update inv.sh
    addded Oracle SOA Suite

--- inv.sh
+++ inv.sh
@@ -7,6 +7,7 @@ total_wls=0
 total_oem=0
 total_nm=0
 total_odi=0
+total_soa=0
 
 for i in $(cat list1.txt);
         do
@@ -34,7 +35,13 @@ for i in $(cat list1.txt);
             echo $i $OS_VER >> ./result.txt
             ODI1=$( SSHPASS=$PASS ./sshpass -e  ssh -o StrictHostKeyChecking=no -o NumberOfPasswordPrompts=1 root@$i "ps aux | grep -i java  | grep weblogic.Server | grep opss.version | grep odi | grep -v AdminServer | grep -v grep | wc -l" ) 2>/dev/null
             total_odi=$(($total_odi + $ODI1))
-			
+
+		    # SOA - Oracle SOA Suite
+            OS_VER=$( SSHPASS=$PASS ./sshpass -e  ssh -o StrictHostKeyChecking=no -o NumberOfPasswordPrompts=1 root@$i " echo 'SOA ' ; ps aux | grep -i java  | grep weblogic.Server | grep jrf.version | grep soa | grep -v AdminServer | grep -v grep | wc -l" ) 2>/dev/null
+            echo $i $OS_VER >> ./result.txt
+            SOA1=$( SSHPASS=$PASS ./sshpass -e  ssh -o StrictHostKeyChecking=no -o NumberOfPasswordPrompts=1 root@$i "ps aux | grep -i java  | grep weblogic.Server | grep jrf.version | grep soa | grep -v AdminServer | grep -v grep | wc -l" ) 2>/dev/null
+            total_soa=$(($total_soa + $SOA1))
+
 done
 
 # print to the screen
@@ -42,6 +49,7 @@ echo "Total WebLogic Server " $total_wls
 echo "Total OEM agent " $total_oem
 echo "Total NodeManager " $total_nm
 echo "Total ODI " $total_odi
+echo "Total SOA " $total_soa
 
 cat ./result.txt
 
@@ -53,4 +61,7 @@ cat ./result.txt
 # "ps aux | grep -i java  | grep weblogic.NodeManager | grep -v grep | wc -l"
 # ODI
 # "ps aux | grep -i java  | grep weblogic.Server | grep opss.version | grep odi | grep -v AdminServer | grep -v grep | wc -l"
-
+# SOA
+# "ps aux | grep -i java  | grep weblogic.Server | grep jrf.version | grep soa | grep -v AdminServer | grep -v grep | wc -l"
+# 
+# 
